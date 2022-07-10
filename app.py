1874c43071068fed9bd3e3ffc9e34b86af61f0b8
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-07-10 14:19:30 -0500

    set up initial flask app config

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 """Flask app for Cupcakes"""
+import os
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
