7bbcb95432ba45f801af5f5cb2199f664f3ec646
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-07-10 18:43:49 -0500

    add javascript to fetch data and render cupcakes on page, create from to add new cupcakes using axios

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,20 +1,42 @@
-/**
-  <li
-              class="list-group-item d-flex justify-content-between align-items-center"
-            >
-              <button class="delete-todo btn-sm btn-danger" data-id="">
-                X
-              </button>
- */
-
 const ul = $("#cupcake-list");
 
 async function getCupcakes() {
+  /*Append cupcakes in db to ul */
   let resp = await axios.get("/api/cupcakes");
-
-  return resp;
+  let cupcakesArray = resp.data.cupcakes;
+  //   console.log(cupcakesArray);
+  cupcakesArray.forEach(cupcake => {
+    flavor = cupcake.flavor.charAt(0).toUpperCase() + cupcake.flavor.slice(1);
+    ul.append(
+      `<li class="list-group-item d-flex align-items-center"><img src="${cupcake.image}" alt="${flavor} cupcake image" class="cupcake-image">
+      ${flavor}</li>`
+    );
+  });
 }
 
-let data = getCupcakes().then(data => console.log(data.data));
+$("#add-btn").on("click", addCupcake);
 
-// console.log(data);
+async function addCupcake(e) {
+  new_cupcake = {
+    flavor: $("#flavor").val(),
+    size: $("#size").val(),
+    rating: $("#rating").val(),
+    image: $("#image").val(),
+  };
+  await axios.post("/api/cupcakes", {
+    flavor: $("#flavor").val(),
+    size: $("#size").val(),
+    rating: $("#rating").val(),
+    image: $("#image").val(),
+  });
+  e.preventDefault();
+}
+
+getCupcakes();
+
+/*
+Add later
+// Rating: ${cupcake.rating}
+// <button class="delete-todo btn-sm btn-danger" data-id="${cupcake.id}">X</button>
+
+*/
